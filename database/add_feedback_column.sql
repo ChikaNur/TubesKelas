c4c9b2e14600c479f0578ab6fd09de3a7cf9a35d
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2026-01-08 05:46:10 +0700

    .

--- database/add_feedback_column.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add feedback column to submissions table
--- Date: 2026-01-08
--- Description: Add feedback column to store instructor feedback for graded assignments
-
-ALTER TABLE submissions ADD COLUMN feedback TEXT AFTER score;
